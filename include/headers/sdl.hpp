5bddd67080b598a6ad078c48a81e7d4a2ee68f76
Tnmae <ttyagi.2505@gmail.com>
2025-10-03 02:21:56 +0000

    defining header

--- include/headers/sdl.hpp
+++ include/headers/sdl.hpp
@@ -2,15 +2,29 @@
 #define SDL_H
 
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_time.h>
 #include <SDL3_image/SDL_image.h>
+#include <iostream>
 #include <string>
+
+static SDL_Window *gWindow = nullptr;
+static SDL_Renderer *gRenderer = nullptr;
 
 class sdl {
 public:
-  void initWindow(std::string title, int height, int width);
-  void initRenderer();
+  SDL_AppResult init(std::string name, std::string version,
+                     std::string identifier, Uint32 flag, std::string title,
+                     Uint32 height, Uint32 width);
+  SDL_AppResult initWindow(std::string title, int height, int width);
+  SDL_AppResult initRenderer();
+  void quit();
+  Uint32 getWindowHeight();
+  Uint32 getWindowWidth();
+  void appLoop();
 
 private:
+  Uint32 width;
+  Uint32 height;
 };
 
 #endif /*SDL_H*/
--- src/sdl.cpp
+++ src/sdl.cpp
@@ -1,5 +1,66 @@
 #include "../include/headers/sdl.hpp"
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_video.h>
 
-void sdl::initWindow(std::string title, int height, int width) {}
+SDL_AppResult sdl::init(std::string name, std::string version,
+                        std::string identifier, Uint32 flag, std::string title,
+                        Uint32 height, Uint32 width) {
+  SDL_SetAppMetadata(name.c_str(), version.c_str(), identifier.c_str());
 
-void sdl::initRenderer() {}
+  if (!SDL_Init(flag)) {
+    std::cerr << "error occured while initialising SDL" << SDL_GetError()
+              << std::endl;
+    return SDL_APP_FAILURE;
+  }
+
+  SDL_ClearError();
+
+  sdl::initWindow(title, height, width);
+
+  sdl::initRenderer();
+
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult sdl::initWindow(std::string title, int height, int width) {
+
+  this->width = width;
+  this->height = height;
+
+  gWindow =
+      SDL_CreateWindow(title.c_str(), width, height, SDL_WINDOW_RESIZABLE);
+  if (!gWindow) {
+    std::cerr << "error creating window" << SDL_GetError() << std::endl;
+    return SDL_APP_FAILURE;
+  } else {
+    std::cout << "window created successfully\n";
+  }
+
+  SDL_ClearError();
+
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult sdl::initRenderer() {
+  gRenderer = SDL_CreateRenderer(gWindow, NULL);
+  if (!gRenderer) {
+    std::cerr << "error creating renderer" << SDL_GetError() << std::endl;
+    return SDL_APP_FAILURE;
+  } else {
+    std::cout << "renderer initialied successfully\n";
+  }
+
+  SDL_ClearError();
+
+  return SDL_APP_CONTINUE;
+}
+
+void sdl::quit() {
+  SDL_DestroyWindow(gWindow);
+  SDL_DestroyRenderer(gRenderer);
+  SDL_DestroyWindowSurface(gWindow);
+}
+
+Uint32 sdl::getWindowHeight() { return this->height; }
+
+Uint32 sdl::getWindowWidth() { return this->width; }
